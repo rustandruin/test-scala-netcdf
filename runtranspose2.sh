1726481b4b2d43377330c358d12337a3a4842c84
Alex Gittens <gittens@icsi.berkeley.edu>
2016-01-08 00:08:25 +0000

    make new tranposition code work?

--- runtranspose2.sh
+++ runtranspose2.sh
@@ -3,7 +3,8 @@
 JARFILE=$1
 ROWCHUNKSBASEFNAME=hdfs:///user/root/CFSRAparquetTranspose/CFSRAparquetTranspose
 NUMROWCHUNKFILES=47
-NUMSUBROWCHUNKS=2
+NUMSUBROWCHUNKS=3 
+# 3 corresponds to transposing about 333 rows of A^T at once
 
 #DIR="$(cd "`dirname "$0"`"; pwd)"
 DIR=/mnt2/climateLogs
@@ -16,13 +17,18 @@ mkdir -p $LOGDIR
 # expects to be run on EC2 in standalone mode with 29-execs on r3.8xlarge instances
 # note maxResultSize needs to be large because are returning large chunks of data (10GB is not sufficient, maybe 13GB might be?)
 
+# 116, 8, 20G
+# 174, 4, 20G # runs faster (maybe 2x) when collecting row names, and also faster when collecting the row vectors
+# 203, 4, 10G # get java heap memory errors
+# 203, 4, 20G # not sure this is any faster than 174, 4, 20G
+# the results of drawing down one chunk of rows (500 rows) is about 103 GB, so set maxResultSize higher
 spark-submit --verbose \
   --driver-memory 220G \
-  --num-executors 116 \
-  --executor-cores 8 \
+  --num-executors 203 \
+  --executor-cores 4 \
   --executor-memory 20G \
-  --driver-java-options '-Dlog4j.configuration=log4j.properties' \
-  --conf "spark.driver.maxResultSize=20G" \
+  --driver-java-options '-Dlog4j.configuration=log4j.properties -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode' \
+  --conf "spark.driver.maxResultSize=120G" \
   --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=log4j.properties" \
   --conf spark.worker.timeout=1200000 \
   --conf spark.network.timeout=1200000 \
--- src/main/scala/transposeAvroToAvroChunks.scala
+++ src/main/scala/transposeAvroToAvroChunks.scala
@@ -4,7 +4,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SQLContext
 
-import scala.math.ceil
+import scala.math.{ceil, Ordering}
 import scala.util.Sorting.quickSort
 import scala.collection.mutable.{ArrayBuffer, WrappedArray}
 
@@ -16,7 +16,7 @@ import org.apache.log4j.Logger
 
 object transposeAvroToAvroChunks {
 
-  private val myLogger = Logger.getLogger("tranpose Avro row chunks to Avro column chunks")
+  private val myLogger = Logger.getLogger("transposeAvro")
 
   def main(args: Array[String]) = {
     val conf = new SparkConf().setAppName("transposeAvroToAvroChunks")
@@ -49,8 +49,8 @@ object transposeAvroToAvroChunks {
     val numSubChunks  = args(4).toInt
 
     // the number of columns in A^T
-    val numCols = sqlContext.read.avro(baseInputFname + "0")
-                  .rdd.first.getAs[WrappedArray[Float]](1).length
+    val numCols = sqlContext.read.avro(baseInputFname + "0").first
+                  .getAs[WrappedArray[Float]](1).length
 
     // keep track of the current chunk of columns of A being computed
     // and all the rows in A^T / columns in A
@@ -61,8 +61,9 @@ object transposeAvroToAvroChunks {
     for( rowChunkIdx <- 0 until numAvroRowChunks ) {
       // get the names of all the rows in this chunk of A^T and sort them
       val rowNames = sqlContext.read.avro(baseInputFname + rowChunkIdx.toString)
-                     .rdd.map( row => row.getAs[String](0)).collect
+                     .select("_1").map( row => row.getAs[String](0)).collect
       val numRowsInChunk = rowNames.length
+      logInfo(s"Collected ${numRowsInChunk} row names")
       quickSort(rowNames)
       allColNames ++= rowNames
       val rowChunks = rowNames.grouped(
@@ -72,24 +73,29 @@ object transposeAvroToAvroChunks {
       // then transpose each piece into a column chunk of A
       for(subRowChunkIdx <- 0 until rowChunks.length) {
         val chunkOfATranspose = 
-          sqlContext.read.avro(baseInputFname + rowChunkIdx.toString).rdd
-            .filter( row => 
-                     rowChunks(subRowChunkIdx) contains row.getAs[String](0) )
+          sqlContext.read.avro(baseInputFname + rowChunkIdx.toString)
+            .filter($"_1" isin(rowChunks(subRowChunkIdx):_*) ).rdd
             .map(row => 
                      Tuple2(row.getAs[String](0), 
                             row.getAs[WrappedArray[Float]](1).toArray))
-            .collect.sortWith{ (left, right) => left._1 < right._1}
-
+            .collect
+        logInfo(s"Collected the ${chunkCounterA} chunk of rows from A^T;" 
+          + s"contains ${chunkOfATranspose.size} rows")
+        quickSort(chunkOfATranspose)(Ordering.by(_._1))
+        logInfo(s"Sorted the rows by increasing date")
         val sortedChunkRowNames = chunkOfATranspose.map(pair => pair._1)
+        logInfo(s"Extracted the row names")
 
         // checks that the row ordering is correct a la 
         // http://blog.bruchez.name/2013/05/scala-array-comparison-without-phd.html
         assert( (sortedChunkRowNames : Seq[String]) ==
                 (rowChunks(subRowChunkIdx) : Seq[String]) )
+        logInfo(s"Asserted that the row ordering is correct")
 
         // contains concat(row1, ..., lastrow) of this chunk of A^T 
         val rowChunksOfATransposeData = 
           chunkOfATranspose.map(pair => pair._2).toArray.flatten
+        logInfo(s"Flattened these rows of A^T into one array")
 
         // Breeze stores matrices in column-major format, 
         // so this implicitly transposes the chunk of A^T to get 
@@ -99,6 +105,7 @@ object transposeAvroToAvroChunks {
           new BDM(numCols, rowChunks(subRowChunkIdx).length, rowChunksOfATransposeData)
           .t.copy.data.grouped(rowChunks(subRowChunkIdx).length).toArray
           .zipWithIndex.map(pair => (pair._2, pair._1))
+        logInfo(s"Transposed the rows of A^T to get columns of A and added row indices")
 
         sc.parallelize(colChunksOfAData).toDF.write
           .avro(baseOutputFname + chunkCounterA.toString)
